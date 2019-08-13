d04dd9d351260c3ac550f6626ab0e0e79b194e79
EHwooKim <ehwoo0707@naver.com>
2019-08-13 14:00:40 +0900

    urls.py 설정 분리

--- first_django/urls.py
+++ first_django/urls.py
@@ -14,31 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from pages import views
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # 1. url 설정
-    # path(url, 해당하는 views의 함수)
-    path('', views.index),
-    # /hello 로 들어오면 인사하기
-    # variable routing
-    # url에 특정 값을 변수처럼 활용!
-    path('hello/<str:name>/', views.hello),
-    # /lotto 로 들어오면 번호 추첨하기
-    path('lotto/', views.lotto),
-    path('dinner/', views.dinner),
-    path('cube/<int:num>/',views.cube),
-    path('about/<str:name>/<int:age>/', views.about),
-    path('isitgwangbok/',views.isitgwangbok),
-    path('ping/', views.ping),
-    path('pong/', views.pong),
-    # 가짜 회원가입. username, password, password_confirmation 받아서 패스워드 일치하면 '00님 회원가입 ok' 아니면 '비번 잘못'
-    path('signup/', views.signup),
-    path('signup_result/', views.signup_result),
-    path('movie/',views.movie),
+    # 각 앱별로 따로 urls.py를 정의하여 관리함.
+    path('pages/', include('pages.urls')),
+    path('services/', include('services.urls'))
 ]
 
---
+++ pages/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views  # 같은 디렉토리에서 views를 가져오겠다! 그래서 . 찍은거야
+
+urlpatterns = [
+    path('', views.index),
+    path('hello/<str:name>/', views.hello),
+    path('lotto/', views.lotto),
+    path('dinner/', views.dinner),
+    path('cube/<int:num>/',views.cube),
+    path('about/<str:name>/<int:age>/', views.about),
+    path('isitgwangbok/',views.isitgwangbok),
+    path('ping/', views.ping),
+    path('pong/', views.pong),
+    path('signup/', views.signup),
+    path('signup_result/', views.signup_result),
+    path('movie/',views.movie),
+]
